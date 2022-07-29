a7d87447ffb38608730ab30b40648e7ec5412e9e
parromd <me@davidparrott.dev>
2022-07-29 10:14:55 -0600

    init

---
+++ src/displayapp/screens/TestApp.cpp
@@ -0,0 +1,20 @@
+#include "displayapp/screens/TestApp.h"
+#include "displayapp/DisplayApp.h"
+
+using namespace Pinetime::Applications::Screens;
+
+TestApp::TestApp(DisplayApp* app) : Screen(app) {
+	lv_obj_t* title = lv_label_create(lv_scr_act(), nullptr);
+	lv_label_set_text_static(title, "test application");
+	lv_label_set_align(title, LV_LABEL_ALIGN_CENTER);
+	lv_obj_align(title, lv_scr_act(), LV_ALIGN_CENTER, 0, 0);
+
+	lv_obj_t* arc = lv_arc_create(lv_scr_act(), nullptr);
+	lv_arc_set_end_angle(arc, 200);
+	lv_obj_set_size(arc, 200, 200);
+	lv_obj_align(arc, NULL, LV_ALIGN_CENTER, 0, 0);
+}
+
+TestApp::~TestApp() {
+	lv_obj_clean(lv_scr_act());
+}
